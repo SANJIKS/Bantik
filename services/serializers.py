1b4991c0f78c698f1c5b12481ab1048d09601986
SANJIK <120570947+SANJIKS@users.noreply.github.com>
2025-03-23 14:07:44 +0600

    Update serializers.py

--- services/serializers.py
+++ services/serializers.py
@@ -7,24 +7,24 @@ User = get_user_model()
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = ["id", "name", "duration"]
+        fields = ["id", "name", "duration", "price"]
 
 class CreateAppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = '__all__'
 
-    # def validate(self, data):
-    #     master = data["master"]
-    #     service = data["service"]
+    def validate(self, data):
+        master = data["master"]
+        service = data["service"]
 
-    #     if service not in master.services.all():
-    #         raise serializers.ValidationError(f"Мастер {master} не предоставляет услугу '{service}'.")
+        if service not in master.services.all():
+            raise serializers.ValidationError(f"Мастер {master} не предоставляет услугу '{service}'.")
 
-    #     if Appointment.objects.filter(master=master, date_time=data["date_time"]).exists():
-    #         raise serializers.ValidationError("Этот мастер уже занят на выбранное время.")
+        if Appointment.objects.filter(master=master, date_time=data["date_time"]).exists():
+            raise serializers.ValidationError("Этот мастер уже занят на выбранное время.")
         
-    #     return data
+        return data
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
